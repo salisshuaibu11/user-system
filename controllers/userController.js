dc84ba7b022b8c8465682e5cdb64f7ea519b1596
salisu <salisshuaibu11@gmail.com>
2021-08-10 09:09:41 +0000

    Implemented get user profile api with its route

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,7 +66,29 @@ const registerUser = asyncHandler(async(req, res) => {
   }
 });
 
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async(req, res) => {
+  const user = await User.findById(req.body._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      city: user.city,
+      stateOfResident: user.stateOfResident
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 module.exports = {
   authUser,
   registerUser,
+  getUserProfile,
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {
   authUser,
   registerUser,
+  getUserProfile,
  } = require("../controllers/userController.js");
 
 router.route("/").post(registerUser);
-router.post("/login", authUser)
+router.post("/login", authUser);
+router.route("/profile").get(getUserProfile);
 module.exports = router;
