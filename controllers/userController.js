86558744d5324d9c822988e5133acb0754a4f2cc
salisu <salisshuaibu11@gmail.com>
2021-08-10 09:45:59 +0000

    Added protect middleware to profile api and also modified req.body to req.user in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const generateToken = require("../utils/generateToken");
-const User = require("../models/userModel.js");
+const User = require("../models/userModel");
 
 // @desc Auth user & get token
 // @route POST /api/users/login
@@ -70,7 +70,7 @@ const registerUser = asyncHandler(async(req, res) => {
 // @route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async(req, res) => {
-  const user = await User.findById(req.body._id);
+  const user = await User.findById(req.user._id);
 
   if (user) {
     res.json({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,8 +5,9 @@ const {
   registerUser,
   getUserProfile,
  } = require("../controllers/userController.js");
+const { protect, } = require("../middleware/authMiddleware");
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(getUserProfile);
+router.route("/profile").get(protect, getUserProfile);
 module.exports = router;
